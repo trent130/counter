ef48f687034e921ea5d0e63fa1c29eebcc27d050
trent130 <72649310+trent130@users.noreply.github.com>
2022-06-27 12:36:53 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,30 @@ button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
+
+const button10 = document.querySelector("#plusTen")
+const indicato = document.querySelector("#number-indicator")
+const dispe = document.querySelector("#display-heading")
+
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+
+const button15 = document.querySelector("#plusFifteen")
+const indicat = document.querySelector("#number-indicator")
+const dispen = document.querySelector("#display-heading")
+
+button15.addEventListener('click', () => {
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+
+const button20 = document.querySelector("#plusTwenty")
+const indica = document.querySelector("#number-indicator")
+const dispens = document.querySelector("#display-heading")
+
+button20.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
+})
